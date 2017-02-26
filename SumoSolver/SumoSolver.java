15de0a91b493e36cf570e1822950fdb20aebbcc7
Lauren Konchan <laurenkonchan@aol.com>
2017-02-25 19:06:48 -0800

    Initialization of SumoSolver problem

---
+++ SumoSolver/SumoSolver.java
@@ -0,0 +1,13 @@
+
+
+public class SumoSolver {
+
+    public static void main (String[] args) {
+      try {
+  			k = Integer.parseInt(args[0]);
+  		} catch (ArrayIndexOutOfBoundsException e) {
+  			System.out.println("BAD DATA");
+  		}
+    }
+
+}
