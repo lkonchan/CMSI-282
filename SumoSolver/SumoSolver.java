bb3d6dd3e7476746815c6ea60e56c3a59e834430
Lauren Konchan <laurenkonchan@aol.com>
2017-02-28 13:06:55 -0800

    working with memoization, needs to hold onto costs that make up the answer.

--- SumoSolver/SumoSolver.java
+++ SumoSolver/SumoSolver.java
@@ -4,70 +4,73 @@ public class SumoSolver {
 
     public static void main (String[] args) {
 
-      int[] cost = {48, 49, 55};
-      int[] weight = {51, 52, 99};
-      int amount = 100;
+      // int[] cost = {100, 49, 55};
+      // int[] weight = {51, 52, 99};
+      // int amount = 100;
+
+
+      int[] cost = new int[args.length];
+      int[] weight = new int[args.length];
+      int amount = 0;
+
+      try {
+          amount = Integer.parseInt(args[args.length - 1]);
+          if (amount < 0) {
+              System.out.println("Amount must be a postive integer.");
+              return;
+          }
+          for (int i = 0; i < args.length-1; i+=2) {
+            cost[i] = Integer.parseInt(args[i]);
+            weight[i] = Integer.parseInt(args[i+1]);
+          }
+      } catch (NumberFormatException nfe) {
+          System.out.println("Amount must be an integer.");
+      }
+
       int[][] solution = new int[cost.length][amount+1];
 
+
       SumoSolver(cost, weight, cost.length-1, amount, solution);
-      System.out.println("Answer is: " + solution[cost.length-1][amount]);
+    //  System.out.println("$" + cost[i] + " / " + weight[i] + " pounds");
+      System.out.println(" items / $" + " / " + solution[cost.length-1][amount] + " pounds");
 
-      // int[] cost = new int[args.length / 2];
-      // int[] weight = new int[args.length / 2];
-      // int amount;
-
-      // try {
-      //     amount = Integer.parseInt(args[args.length - 1]);
-      //     if (amount < 0) {
-      //         System.out.println("Amount must be a postive integer.");
-      //         return;
-      //     }
-      // } catch (NumberFormatException nfe) {
-      //     System.out.println("Amount must be an integer.");
-      // }
-
-      // for (int i = 0; i < args.length-1; i++) {
-      //   if (i%2 == 0) {
-      //
-      //   }
-      // }
-
-      // example code for parsing args
-      // try {
-  		// 	k = Integer.parseInt(args[0]);
-  		// } catch (ArrayIndexOutOfBoundsException e) {
-  		// 	System.out.println("BAD DATA");
-  		// }
     }
 
-    //returns array with total cost, which foods were used & their costs & weights
-    //array[cost[], weight[], foods[], weights[]]; where cost & weight only have 1 item
-    //table
 
     public static int SumoSolver(int[] cost, int[] weight, int i, int j, int[][]solution) {
-      //System.out.println("conditional: " + (j-cost[i]));
-      //System.out.println(j + " " + cost[i]);
+      //conditon where we can pull a cost out and we are not on the top row of the table
       if (j - cost[i] > 0 && i > 0) {
-        ////System.out.println(solution[i][j]);
-        int supplement = SumoSolver(cost, weight, i, j-cost[i], solution);
-        System.out.println("weight = " + weight[i] + " supplement = " + supplement + " cost = " + cost[i]);
-        int lbs = weight[i] + supplement;
-        int previousSolution = SumoSolver(cost, weight, i-1, j, solution);
-        if (lbs > previousSolution) {
-          solution[i][j] = lbs;
-          //System.out.println("switched to supplement: " + solution[i][j]);
-          return lbs;
+        int lbs;
+        int previousSolution;
+
+        //memoization of supplemental value
+        if (solution[i-1][j-cost[i]] != 0) {
+          lbs = weight[i] + solution[i-1][j-cost[i]];
         } else {
-          solution[i][j] = previousSolution;
-          //System.out.println("used previous solution: " +solution[i][j]);
-          return previousSolution;
+          lbs = weight[i] + SumoSolver(cost, weight, i-1, j-cost[i], solution); //supplement
         }
+
+        //memoization of previous solution
+        if (solution[i-1][j] != 0) {
+          previousSolution = solution[i-1][j];
+        } else {
+          previousSolution = SumoSolver(cost, weight, i-1, j, solution);
+        }
+
+        //check which solution is better
+        if (lbs > previousSolution) {
+          return solution[i][j] = lbs;
+        } else {
+          return solution[i][j] = previousSolution;
+        }
+
+      //condition where we can pull a cost out but we are on the top row of the table
       } else if (j - cost[i] > 0 && j >= 0) {
-        System.out.println("Weight: " + weight[i]);
         solution[i][j] = weight[i];
         return solution[i][j];
+
+      //condition where we cannot pull a cost out and we are on the top row
       } else {
-        //System.out.println(solution[i][j]);
         return 0;
       }
     }
